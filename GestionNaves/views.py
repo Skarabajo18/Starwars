503d3efbb6d5038a8cf434a91caa4171520afa77
Skarabajo18 <felipesegovia14@gmail.com>
2023-04-11 02:08:50 -0400

    update

--- GestionNaves/views.py
+++ GestionNaves/views.py
@@ -1,17 +1,13 @@
 from django.shortcuts import render
-from .models import Nave,Modelo,Piloto,DueñoNave
-from django.views import generic 
-from django.http import HttpResponse
-from django.views.generic import ListView, DetailView
-from django.template import loader, Context
-from django.shortcuts import render
-from django.shortcuts import render_to_response, get_object_or_404
+from .models import Nave, Modelo, Piloto, DueñoNave
+
 from django.http import HttpResponseRedirect
 from .Form import Formulario
 from django.contrib.auth.models import User, Group
 from rest_framework import viewsets
-from .serializers import UserSerializer, GroupSerializer,NaveSerializer,ModeloSerializer,PilotoSerializer
+from .serializers import UserSerializer, GroupSerializer, NaveSerializer, ModeloSerializer, PilotoSerializer
 # Create your views here.
+
 
 def app(request):
     # utilisamos el comando .all().count() para poder contar todos las naves que tenemos
@@ -21,86 +17,111 @@ def app(request):
     num_dueñoN = DueñoNave.objects.all().count()
 
     return render(
-        #retornamos un render para renderisar 3 pedidos que son el del request el de la pagina que queremos llamar y el contexto del que queremos agregar.
+        # retornamos un render para renderisar 3 pedidos que son el del request el de la pagina que queremos llamar y el contexto del que queremos agregar.
         request,
         'app.html',
-        context={'num_nave':num_nave,'num_pilotos':num_pilotos,'num_modelo':num_modelo,'num_dueñoN':num_dueñoN },
+        context={'num_nave': num_nave, 'num_pilotos': num_pilotos,
+                 'num_modelo': num_modelo, 'num_dueñoN': num_dueñoN},
     )
+
+
 def index(request):
     return render(
-        #renderisamos el pedido que es llamar a la  pagina index.html
+        # renderisamos el pedido que es llamar a la  pagina index.html
         request,
         "index.html"
     )
+
+
 def pag2(request):
     return render(
         request,
         "pag2.html"
     )
+
+
 def Minijuego2(request):
     return render(
         request,
         "Minijuego2.html"
     )
+
+
 def enContru(request):
     return render(
         request,
         "enContru.html"
     )
+
+
 def NaveLista(request):
-    #llamamos a todos las naves que estan en la base de datosla base de datos Nave.objects.all()
+    # llamamos a todos las naves que estan en la base de datosla base de datos Nave.objects.all()
     nave = Nave.objects.all()
-    #en el constexto ingresamos la manera de como llamar todos los datos para la lista
-    contexto = {'Naves':nave}
+    # en el constexto ingresamos la manera de como llamar todos los datos para la lista
+    contexto = {'Naves': nave}
     return render(
         request,
         'Nave.html',
         contexto
     )
+
+
 def PilotoLista(request):
-    #llamamos a todos los pilotos que estan en la base de datosla base de datos Nave.objects.all()
+    # llamamos a todos los pilotos que estan en la base de datosla base de datos Nave.objects.all()
     pilote = Piloto.objects.all()
-    contexto = {'Pilotos':pilote}
+    contexto = {'Pilotos': pilote}
     return render(
         request,
         'piloto.html',
         contexto
     )
+
+
 def ModeloLista(request):
     modele = Modelo.objects.all()
-    contexto = {'Modelos':modele}
+    contexto = {'Modelos': modele}
     return render(
         request,
         'Modelo.html',
         contexto
     )
+
+
 def DueñoLista(request):
     dueñe = DueñoNave.objects.all()
-    contexto = {'Dueño':dueñe}
+    contexto = {'Dueño': dueñe}
     return render(
         request,
         'duennos.html',
         contexto
     )
+
+
 def admin(request):
     return render(
         request,
         "http://127.0.0.1:8000/admin"
     )
+
+
 def contacto(request):
-    if request.method == 'POST': #Si el formulario es enviado#
+    if request.method == 'POST':  # Si el formulario es enviado#
         form = Formulario(request.POST)
-        if form.is_valid():#Si es valido se procesan los datos
-            return HttpResponseRedirect ('GestionNaves/templates/') #y se redirige a la url
+        if form.is_valid():  # Si es valido se procesan los datos
+            # y se redirige a la url
+            return HttpResponseRedirect('GestionNaves/templates/')
     else:
-        form = Formulario() #formulario vacio
-    return render(request, 'post.html',{'form': form,})
+        form = Formulario()  # formulario vacio
+    return render(request, 'post.html', {'form': form, })
+
+
 def gracias(request):
-   return render(
-        #renderisamos el pedido que es llamar a la  pagina index.html
+    return render(
+        # renderisamos el pedido que es llamar a la  pagina index.html
         request,
         "index.html"
     )
+
 
 class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all().order_by('-date_joined')
@@ -111,13 +132,16 @@ class GroupViewSet(viewsets.ModelViewSet):
     queryset = Group.objects.all()
     serializer_class = GroupSerializer
 
+
 class NaveViewSet(viewsets.ModelViewSet):
     queryset = Nave.objects.all()
     serializer_class = NaveSerializer
 
+
 class ModeloViewSet(viewsets.ModelViewSet):
     queryset = Modelo.objects.all()
     serializer_class = ModeloSerializer
+
 
 class PilotoViewSet(viewsets.ModelViewSet):
     queryset = Piloto.objects.all()
